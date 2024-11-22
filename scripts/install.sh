504f7f4d25dc2b255f8f2d74dad5fd280e21c42b
Alex Potapov <potapov@yahoo.com>
2024-11-22 15:26:53 -0500

    Initial

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,3 +2,5 @@
 
 curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
 sed -i 's|#force_color_prompt=yes|force_color_prompt=yes|g' /root/.bashrc
+mkdir -p /root/.kube
+cp /etc/rancher/k3s/k3s.yaml /root/.kube/config
