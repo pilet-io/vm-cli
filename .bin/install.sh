a91dc4284996d28c1870eda82c8e219aaa662388
Alex Potapov <potapov@yahoo.com>
2025-02-05 07:22:17 -0500

    Initial

---
+++ .bin/install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+git clone https://github.com/pilet-io/vm-cli.git
+
+mv /root/vm-cli /root/cli
+echo "export PATH=\$PATH:/root/cli" >> /root/.profile
+/root/cli/with host setup completion
+
+curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC='server --tls-san system.kueb.io --flannel-ipv6-masq --cluster-cidr 10.42.0.0/16,2001:cafe:42:0::/56 --service-cidr 10.43.0.0/16,2001:cafe:43:1::/112' sh -s -
+curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
