bb20b20a2dd8546eb9ca83c19356185b660a8a60
Alex Potapov <potapov@yahoo.com>
2025-02-06 08:27:48 -0500

    Initial

--- .bin/install.sh
+++ .bin/install.sh
@@ -12,3 +12,59 @@ curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
 wget https://github.com/open-telemetry/opentelemetry-collector-releases/releases/download/v0.119.0/otelcol-contrib_0.119.0_linux_amd64.deb
 dpkg -i otelcol-contrib_0.119.0_linux_amd64.deb
 rm otelcol-contrib_0.119.0_linux_amd64.deb
+
+cat <<EOF > /etc/otelcol-contrib/config.yaml
+extensions:
+  health_check:
+  pprof:
+    endpoint: 0.0.0.0:1777
+  zpages:
+    endpoint: 0.0.0.0:55679
+
+receivers:
+  hostmetrics:
+    collection_interval: 30s
+    scrapers:
+      cpu:
+      memory:
+      disk:
+  otlp:
+    protocols:
+      grpc:
+        endpoint: 0.0.0.0:4317
+      http:
+        endpoint: 0.0.0.0:4318
+
+processors:
+  batch:
+  resource:
+    attributes:
+    - key: "host.name"
+      value: "SYS_HOSTNAME"
+      action: upsert
+
+exporters:
+  otlphttp:
+    endpoint: https://system.kueb.io:4318
+
+service:
+
+  pipelines:
+
+    traces:
+      receivers: [otlp]
+      processors: [batch]
+      exporters: [otlphttp]
+
+    metrics:
+      receivers: [otlp, hostmetrics]
+      processors: [resource,batch]
+      exporters: [otlphttp]
+
+    logs:
+      receivers: [otlp]
+      processors: [batch]
+      exporters: [otlphttp]
+
+  extensions: [health_check, pprof, zpages]
+EOF
